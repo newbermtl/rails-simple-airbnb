cb287a67aac4f409c938492f3473048163ec443f
Frederic Lauzier <lauzier.frederic@gmail.com>
2021-02-09 02:14:35 -0500

    Added flats cards for the index page, added simple form to generate a form that allows us to create new flats (haven't added image addition yet), began show page => @app is acessible just need to find a styling for the page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,25 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save!
+      redirect_to flats_path
+    end
+  end
+
+  private
+
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests )
+    end
+
 end
